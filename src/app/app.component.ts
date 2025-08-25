490625f94dab8369ef21ffa0c1117e27d01d8278
matigous <matheus.vilela.898@gmail.com>
2025-08-25 20:38:49 -0300

    feat: implement language-switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
+import { LanguageSwitcherComponent } from './components/language-switcher/language-switcher.component';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    CommonModule,
+    TranslateModule,
+    LanguageSwitcherComponent
+  ],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'ShopBox';
+  public authService = inject(AuthService);
+  public cartService = inject(CartService);
+
+  constructor(
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('pt');
+    this.translate.use('pt');
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
